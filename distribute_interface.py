daaf3170a4ee5497fe03c2c9e59c2124e511bdc8
FlowerSeeFlowerOpen <wuyonghan.work@outlook.com>
2026-03-10 11:23:26 +0800

    增加分发代码

---
+++ distribute_interface.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+
+stable_interface_list = ["luyaokun", "gaofangzhou"]
+stable_interface_pro_list = ["fuliming"]
+beta_interface_list = ["zhangsan"]
+temporary_interface_list = ["lisi"]
+discarded_name_list = ["wangwu"]
+backlist = ["zhaoliu"]
+summary_name_list = tuple(stable_interface_list + stable_interface_pro_list + beta_interface_list + temporary_interface_list)
+
+
+def distribute_interface():
+    pass
+
+
+if __name__ == "__main__":
+    distribute_interface()
